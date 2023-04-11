2b1c7e3db33bcc15f7c351fd6585f2beae394acb
Gopi Kapadia <123082735+grk2188@users.noreply.github.com>
2023-04-11 18:19:33 -0400

    Add files via upload

---
+++ word replacement.py
@@ -0,0 +1,10 @@
+
+
+def replace_word():
+    str = "Hello, Hope you are doing great! ok ok. "
+    word_to_replace = input("Enter word to replace: ")
+    word_replacement = input("Enter word name to add: ")
+
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
